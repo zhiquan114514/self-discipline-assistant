07baec870eb64e3f8bc5983e1d98ef3ff1beacd0
zhiquan114514 <zhiquan@email.sc.edu>
2022-03-05 05:29:27 -0500

    timer finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,7 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: 'message/:id',
-    loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
-  },
+ 
   {
     path: '',
     redirectTo: 'home',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 import { DataService, Message } from '../services/data.service';
 
 @Component({
@@ -7,16 +8,56 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private data: DataService) {}
 
-  refresh(ev) {
-    setTimeout(() => {
-      ev.detail.complete();
-    }, 3000);
+  private subscription: Subscription;
+  public showInput = true;
+
+    public processName;
+    public seconds;
+    public minutes;
+    public hours;
+
+
+
+    private countdown () {        
+      if(this.seconds != 0)
+        this.seconds -=1;
+      else if(this.seconds == 0 && this.minutes != 0) {
+        this.seconds = 59;
+        this.minutes -=1;
+      } else if(this.minutes == 0 && this.hours != 0){
+        this.seconds = 59
+        this.minutes = 59;
+        this.hours -= 1;
+      } else{
+        this.showInput = true;
+
+        console.log("send")
+        this.subscription.unsubscribe()
+      }
+    }
+
+
+  start(){
+    this.showInput = false;
+
+         this.subscription = interval(1000)
+      .subscribe(x => { this.countdown(); });
+  }
+  Stop(){
+    this.subscription.unsubscribe();
+    this.seconds = 0;
+    this.minutes = 0;
+    this.hours = 0
+    this.showInput = true;
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
+    ngOnInit() {
+
+    }
+
+   ngOnDestroy() {
+      this.subscription.unsubscribe();
+   }
 
 }
